209471dbb6be2652fdd364f2d30c1c55770357e1
Mauro <mbvcode@gmail.com>
2023-11-10 11:52:43 -0300

    Add new icons for clipboard list and dollar

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -127,3 +127,15 @@ export const ArrowDownIcon = (props) => (
     <path d='m6 9 6 6 6-6' />
   </svg>
 )
+
+export const ClipBoardListIcon = ({ className }) => (
+  <svg className={`w-6 h-6 text-gray-800 ${className}`} aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='18' height='20' fill='none' viewBox='0 0 18 20'>
+    <path stroke='currentColor' strokeLinecap='round' strokeWidth='2' d='M12 2h4a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4m6 0v3H6V2m6 0a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1M5 5h8m-5 5h5m-8 0h.01M5 14h.01M8 14h5' />
+  </svg>
+)
+
+export const DollarIcon = ({ className }) => (
+  <svg className={`w-6 h-6 text-gray-800 ${className}`} aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 11 20'>
+    <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M1.75 15.363a4.954 4.954 0 0 0 2.638 1.574c2.345.572 4.653-.434 5.155-2.247.502-1.813-1.313-3.79-3.657-4.364-2.344-.574-4.16-2.551-3.658-4.364.502-1.813 2.81-2.818 5.155-2.246A4.97 4.97 0 0 1 10 5.264M6 17.097v1.82m0-17.5v2.138' />
+  </svg>
+)
