ce4d3a8e384dff29bdb1d1bccc6144b5b858e346
Mauro <mbvcode@gmail.com>
2023-12-22 13:31:10 -0300

    Add link to home page in NotFoundPage component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,6 @@
 import dogImage from '../assets/images/dog_sleeping.png'
 import { Blob } from '../components/Blob'
+import { Link } from 'react-router-dom'
 
 export function NotFoundPage () {
   return (
@@ -18,11 +19,11 @@ export function NotFoundPage () {
             </p>
           </div>
           <div className='flex justify-center'>
-            <button className='relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
+            <Link to='/' className='relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
               <span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
                 Ir a la página principal
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div className='relative flex items-center'>
